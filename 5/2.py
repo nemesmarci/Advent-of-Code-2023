e218d5b1c557ecfc9c5bf13d3d2cac83f24848f7
Nemes Marcell <nemes@sch.bme.hu>
2023-12-05 17:22:11 +0000

    Refactor part2

--- 5/2.py
+++ 5/2.py
@@ -5,46 +5,47 @@ def intersection(r1, r2):
     return range(max(r1.start, r2.start), min(r1.stop, r2.stop))
 
 
-def sort_by_src(r):
-    return sorted(r, key=lambda x: x[0].start)
-
-
-def transform_ranges(ranges, rules):
+def transform_ranges(original_ranges, rules):
     new_ranges = []
-    rules = sort_by_src(rules)
-    for r in ranges:
-        if r.start >= rules[-1][0].stop:
-            new_ranges.append(r)
+    for source_range in original_ranges:
+        if source_range.start >= rules[-1][0].stop:
+            new_ranges.append(source_range)
             continue
-        first = next(i for i, rule in enumerate(rules) if r.start in rule[0])
+        first = next(i for i, rule in enumerate(rules)
+                     if source_range.start in rule[0])
         for i in range(first, len(rules)):
-            start_offset = max(r.start, rules[i][0].start) - rules[i][0].start
-            if r.stop in rules[i][0]:
-                end_offset = r.stop - rules[i][0].start
-                new_ranges.append(range(rules[i][1].start + start_offset,
+            new_start = (rules[i][1].start +
+                         max(source_range.start, rules[i][0].start) -
+                         rules[i][0].start)
+            if source_range.stop in rules[i][0]:
+                end_offset = source_range.stop - rules[i][0].start
+                new_ranges.append(range(new_start,
                                         rules[i][1].start + end_offset))
                 break
             else:
-                new_ranges.append(range(rules[i][1].start + start_offset,
-                                        rules[i][1].stop))
+                new_ranges.append(range(new_start, rules[i][1].stop))
     return [r for r in new_ranges if r]
+
+
+def add_missing_ranges(rules):
+    rules.sort(key=lambda x: x[0].start)
+    missing_ranges = []
+    prev_stop = 0
+    for src_range in (rule[0] for rule in rules):
+        if prev_stop != src_range.start:
+            missing_ranges.append((range(prev_stop, src_range.start),
+                                   range(prev_stop, src_range.start)))
+        prev_stop = src_range.stop
+    rules.extend(missing_ranges)
+    rules.sort(key=lambda x: x[0].start)
 
 
 with open('input.txt') as data:
     seeds, map_ranges = parse(data)
-
-
-for rr in map_ranges:
-    source_ranges = [x[0] for x in sort_by_src(rr)]
-    prev_stop = 0
-    for r in source_ranges:
-        if prev_stop != r.start:
-            rr.append([range(prev_stop, r.start), range(prev_stop, r.start)])
-        prev_stop = r.stop
-
-target_ranges = [range(start, start + r)
-                 for start, r in zip(seeds[0::2], seeds[1::2])]
-for m in map_ranges:
-    target_ranges = transform_ranges(target_ranges, m)
-
+for rules in map_ranges:
+    add_missing_ranges(rules)
+target_ranges = [range(start, start + length)
+                 for start, length in zip(seeds[0::2], seeds[1::2])]
+for rules in map_ranges:
+    target_ranges = transform_ranges(target_ranges, rules)
 print(min(r.start for r in target_ranges))

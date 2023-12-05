6bd0de29532616badf3e2052f395c1ff489451e7
Nemes Marcell <nemes@sch.bme.hu>
2023-12-05 16:13:31 +0000

    Refactor part1

--- 5/1.py
+++ 5/1.py
@@ -3,10 +3,11 @@ from common import parse
 with open('input.txt') as data:
     seeds, map_ranges = parse(data)
 
-locations = []
+min_loc = None
 for target in seeds:
     for m in map_ranges:
         target = next((dest[target - source.start] for source, dest in m
                        if target in source), target)
-    locations.append(target)
-print(min(locations))
+    if min_loc is None or target <= min_loc:
+        min_loc = target
+print(min_loc)
